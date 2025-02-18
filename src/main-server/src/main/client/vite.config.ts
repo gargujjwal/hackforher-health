45df4a0a88f69e2c5acaae01385f7bf496a2e326
Ujjwal Garg <garg.ujjwal04@gmail.com>
2025-02-18 10:19:45 +0530

    dockerise the application

--- src/main-server/src/main/client/vite.config.ts
+++ src/main-server/src/main/client/vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
-    outDir: "../resources/static",
+    outDir: "./dist",
     emptyOutDir: true,
   },
 });
